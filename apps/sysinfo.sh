a6fe0158a11bc496031680f4dee24f2a79092aea
bradbrownjr <92752598+bradbrownjr@users.noreply.github.com>
2022-09-25 00:35:21 -0400

    Update sysinfo.sh
    Added comments

--- apps/sysinfo.sh
+++ apps/sysinfo.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# Install Neofetch with 'sudo apt-get install neofetch'
+
+# OS logo and system information
 neofetch
+
+# Disk usage
 df /
 echo
+# Confirm required node processes are running
 ps -A | grep -E 'direwolf|linbpq' | column -t
 
