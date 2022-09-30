c93df8f44c9421a58576889f6e1ebf5d62b36efd
bradbrownjr <92752598+bradbrownjr@users.noreply.github.com>
2022-09-29 23:08:44 -0400

    Removed w3m req, added function
    Removed the requirement for the w3m package to be installed
    No OS calls, uses native functions
    Created expandable function for outputting results

--- apps/space.py
+++ apps/space.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import os
+import requests
 menu = """
 Space Weather Reports
-------------------------------------------
+-----------------------------------
 1) Forecast Discussion
 2) 3-Day Forecast
 3) 3-Day Geomagnetic Forecast
@@ -10,12 +10,7 @@ Space Weather Reports
 5) Advisory Outlook
 6) Weekly Highlights and Forecasts
 7) Geophysical Alert Message
-------------------------------------------
-9) About this application
-0) Return to node
-
-Information is retrieved from https://www.swpc.noaa.gov/
-"""
+-----------------------------------"""
 
 about = """
 This is a Python3 script which pulls data from
@@ -27,26 +22,32 @@ is blank or cuts off too early, you may have encountered the
 BBS node's app timeout. Try again if nothing was retrieved.
 """
 
-print (menu)
-while True:
-        selected = int(input ("Select an application: "))
+def pullthis(url):
+        response = requests.get(url)
+        data = response.text
+        print("\n{}\n".format(data))
 
-        if selected == 1:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/discussion.txt -dump")
-        elif selected == 2:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/3-day-forecast.txt -dump")
-        elif selected == 3:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/3-day-geomag-forecast.txt -dump")
-        elif selected == 4:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/3-day-solar-geomag-predictions.txt -dump")
-        elif selected == 5:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/advisory-outlook.txt -dump")
-        elif selected == 6:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/weekly.txt -dump")
-        elif selected == 7:
-                os.system("/usr/bin/w3m https://services.swpc.noaa.gov/text/wwv.txt -dump")
-        elif selected == 9:
+print(menu)
+while True:
+        selected = str(input("#1-7), R)elist, A)bout, Q)uit :> "))
+        if "1" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/discussion.txt")
+        elif "2" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/3-day-forecast.txt")
+        elif "3" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/3-day-geomag-forecast.txt")
+        elif "4" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/3-day-solar-geomag-predictions.txt")
+        elif "5" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/advisory-outlook.txt")
+        elif "6" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/weekly.txt")
+        elif "7" in selected:
+                pullthis("https://services.swpc.noaa.gov/text/wwv.txt")
+        elif "a" in selected.lower():
                 print (about)
-        elif selected == 0:
+        elif "r" in selected.lower():
+                print (menu)
+        elif "q" in selected.lower():
                 print ("\nExiting...\n")
-                exit()          
+                exit()
