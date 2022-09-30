ac0bee72bc862d9d2fcbf108a723887fd57b3d58
bradbrownjr <92752598+bradbrownjr@users.noreply.github.com>
2022-09-29 23:24:06 -0400

    Removed w3m system call, internal functions

--- apps/wx.py
+++ apps/wx.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# This script requires w3m which may not be installed by default on your OS
-# sudo apt-get install w3m
-
-import os
+import requests
 menu = """
 Weather Services
 ------------------------------------------
@@ -10,42 +7,38 @@ Weather Services
 2) Maine/New Hampshire Weather Roundup
 3) Western Maine/New Hampshire Forecast
 4) Northern and Eastern Maine Forecast
-5) Maine/New Hampshire Max/Min Temperature 
+5) Maine/New Hampshire Max/Min Temperature
    and Precipitation Table
-------------------------------------------
-9) About this application
-0) Return to node
-
-Information is retrieved from 
-https://www.maine.gov/mema/weather/general-information
-"""
+------------------------------------------"""
 
 about = """
-This is a Python3 script which pulls data from weather.gov
-reports and displays them on the screen using the w3m -dump
-utility.
-
-Enter the number of the report to retrieve it. If the report
-is blank or cuts off too early, you may have encountered the
-BBS node's app timeout. Try again if nothing was retrieved.
+This is a Python3 script which pulls data from
+https://www.maine.gov/mema/weather/general-information
+Script developed by Brad Brown KC1JMH
 """
+
+def pullthis(url):
+        response = requests.get(url)
+        data = response.text
+        print("\n{}\n".format(data))
 
 print (menu)
 while True:
-	selected = int(input ("Select an application: "))
-
-	if selected == 1:
-        	os.system("/usr/bin/w3m https://w1.weather.gov/data/GYX/RWSGYX -dump")
-	elif selected == 2:
-        	os.system("/usr/bin/w3m http://www.weather.gov/data/GYX/RWRGYX -dump")
-	elif selected == 3:
-        	os.system("/usr/bin/w3m http://www.weather.gov/data/GYX/SFTGYX -dump")
-	elif selected == 4:
-        	os.system("/usr/bin/w3m http://www.weather.gov/data/CAR/SFTCAR -dump")
-	elif selected == 5:
-        	os.system("/usr/bin/w3m http://www.weather.gov/data/GYX/RTPGYX -dump")
-	elif selected == 9:
-		print (about)
-	elif selected == 0:
-		print ("\nExiting...\n")
-		exit()
+        selected = str(input("#1-5), R)elist, A)bout, Q)uit :> "))
+        if "1" in selected:
+                pullthis("https://w1.weather.gov/data/GYX/RWSGYX")
+        elif "2" in selected:
+                pullthis("/usr/bin/w3m http://www.weather.gov/data/GYX/RWRGYX")
+        elif "3" in selected:
+                pullthis("http://www.weather.gov/data/GYX/SFTGYX")
+        elif "4" in selected:
+                pullthis("http://www.weather.gov/data/CAR/SFTCAR")
+        elif "5" in selected:
+                pullthis("http://www.weather.gov/data/GYX/RTPGYX")
+        elif "a" in selected.lower():
+                print (about)
+        elif "r" in selected.lower():
+                print (menu)
+        elif "q" in selected.lower():
+                print ("\nExiting...\n")
+                exit()
