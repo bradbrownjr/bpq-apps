b86c9e019b277de616ad8ffcb1b797ff5e502b18
bradbrownjr <92752598+bradbrownjr@users.noreply.github.com>
2022-10-02 01:15:03 -0400

    Update qrz3.py

--- apps/qrz3.py
+++ apps/qrz3.py
@@ -187,11 +187,11 @@ def main():
     while True:
         callsign = input(Colors.BLUE + '\nCallsign: ' + Colors.END)
         if "?" in callsign or "h"in callsign.lower() or "help" in callsign.lower():
-            print("Enter callsign or leave blank to quit")
-        elif "" not in callsign:
-            lookup_callsign(callsign, session_key)
-        else:
+            print("Enter callsign or enter 'q' to quit")
+        elif "q" in callsign.lower() or "quit" in callsign.lower() or "x" in callsign.lower():
             exit()
+        else:
+            lookup_callsign(callsign, session_key)
 
 if __name__ == "__main__":
     exit (main())
